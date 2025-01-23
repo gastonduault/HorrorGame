4323b94393e081b316715129633a4e4ee1541368
Camille Berquet <camille.berquet@united-vr.com>
2025-01-24 00:18:23 +0100

    Action grab the cover box with feedback

---
+++ Assets/Scripts/ParticleEffectManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleEffectManager : MonoBehaviour
+{
+    public GameObject particleEmmiterPrefab;
+    private GameObject instance;
+    // Start is called before the first frame update
+    public void Activate()
+    {
+        instance = Instantiate(particleEmmiterPrefab, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    public void Desactivate()
+    {
+        Destroy(instance);
+    }
+}
