8a0242431a3ccb972e391e8d0343c48d64adc879
julgndr16 <julegendre16@gmail.com>
2025-02-07 15:47:00 +0100

    add mirror OK

--- Assets/Scripts/MirrorBreak.cs
+++ Assets/Scripts/MirrorBreak.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 
 /****************************************
  * Author: Julie
- * Date: 14/01/22025
+ * Date: 14/01/2025
  * General description: This script causes a mirror to shatter on impact with the ground. The intact mirror is replaced by a broken version, and physics is activated on the broken pieces to simulate the breaking effect.
  ****************************************/
 public class MirrorBreak : MonoBehaviour
@@ -13,11 +14,18 @@ public class MirrorBreak : MonoBehaviour
 
     public GameObject glass;
     public GameObject brokenGlass;
+    public Rigidbody rb;
+    public XRGrabInteractable grabInteractable;
+
+    public Vector3 offsetOnGrab = new Vector3(0, 0, 0.2f); // Décalage en avant
+
     void Start()
     {
         //Set the broken glass to be inactive at the start of the game
         glass.SetActive(true);
         brokenGlass.SetActive(false);
+        rb = GetComponent<Rigidbody>();
+        grabInteractable = GetComponent<XRGrabInteractable>();
     }
 
     void OnCollisionEnter(Collision collision)
@@ -25,7 +33,18 @@ public class MirrorBreak : MonoBehaviour
         BreakMirror();
     }
 
+    public void OnReleased(XRBaseInteractor interactor)
+    {
+        Transform interactorTransform = interactor.transform;
+        glass.transform.position = interactorTransform.position + interactorTransform.forward * offsetOnGrab.z
+                                  + interactorTransform.right * offsetOnGrab.x
+                                  + interactorTransform.up * offsetOnGrab.y;
+        rb.useGravity = true;
+    }
+
     void BreakMirror(){
+        brokenGlass.transform.position = glass.transform.position;
+        brokenGlass.transform.rotation = glass.transform.rotation;
         glass.SetActive(false);
         brokenGlass.SetActive(true);
         Rigidbody[] rbs = brokenGlass.GetComponentsInChildren<Rigidbody>();
